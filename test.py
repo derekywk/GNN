e8d93022170c4a7ec80eb76445d6c2cfb34a5428
derek.yuen@wizpresso.com <6aa8a22d0ae819b52740c03a6a56b016411c581b>
2022-07-02 19:34:54 +0800

    - Word2Vec Models - Cached Amazon Data

--- test.py
+++ test.py
@@ -5,31 +5,40 @@ import spacy
 import numpy as np
 import gensim
 from collections import Counter
-from gensim import corpora, models
-ds, info = tfds.load('amazon_us_reviews/Watches_v1_00', split='train', with_info=True)
-# ds, info = tfds.load('amazon_us_reviews/Shoes_v1_00', split='train', with_info=True)
-df = pd.DataFrame(tfds.as_dataframe(ds, info))
-df = df.rename({col: col.replace('data/', '') for col in df.columns}, axis=1)
-df['review_body'] = df['review_body'].str.decode('utf-8')
-genuine = (df['helpful_votes'] / df['total_votes']) >= 0.7
-fraud = (df['helpful_votes'] / df['total_votes']) <= 0.3
-target_df = df.loc[genuine|fraud]
-print("Size of Dataset", df.shape)
-print(f"Genuine Count: {sum(genuine)} ({sum(genuine)/len(target_df)}); Fraud Count: {sum(fraud)} ({sum(fraud)/len(target_df)}); ratio: {sum(genuine)/sum(fraud)}:1")
+from gensim.models import Word2Vec, KeyedVectors
+import gensim.downloader as api
+from datetime import datetime
+import nltk
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+
+DATASET = "Watches_v1_00"
+# DATASET = "Shoes_v1_00"
+DATASET_SIZE = -1
+
+GENUINE_THRESHOLD = 0.7
+FRAUDULENT_THRESHOLD = 0.3
+
+MODEL_NAME = f"Word2Vec_{DATASET}_size_{DATASET_SIZE}"
+
+def tprint(*args):
+    print(datetime.now(), *args)
+
+def wordnet_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
 # WATCH DATASET
 # Size of Dataset (960872, 15)
 # Genuine Count: 247142 (0.2572059545912463); Fraud Count: 73549 (0.07654401418711337); ratio: 3.360236033120777:1
-
-def keyword_helpful_count(keyword, latex=True):
-    keyword_df = target_df.loc[target_df['review_body'].str.contains(keyword, case=False)]
-    genuine = (keyword_df['helpful_votes'] / keyword_df['total_votes']) >= 0.7
-    fraud = (keyword_df['helpful_votes'] / keyword_df['total_votes']) <= 0.3
-    if latex:
-        print(fr"{keyword[2:]} & {len(keyword_df)} ({round(len(keyword_df)/len(target_df)*100, 2)}\%) & {sum(genuine)} & {sum(fraud)} & {round(sum(genuine)/sum(fraud), 2)}:1")
-    else:
-        print(f"{{{keyword}}}: Keyword Count: {len(keyword_df)} ({len(keyword_df)/len(target_df)}); Genuine Count: {sum(genuine)} ({sum(genuine)/len(target_df)}); Fraud Count: {sum(fraud)} ({sum(fraud)/len(target_df)}); ratio: {sum(genuine)/sum(fraud)}:1")
-    return sum(genuine), sum(genuine)/len(target_df), sum(fraud), sum(fraud)/len(target_df)
 
 def word_count(n=100):
     counter = Counter(' '.join(df['review_body']).lower().split())
@@ -70,11 +79,106 @@ def keyword_extraction():
     custom_kw_extractor = yake.KeywordExtractor(lan='en', n=2, dedupLim=0.9, top=20, features=None)
     keywords = custom_kw_extractor.extract_keywords('\n'.join(df['review_body']))
 
-def word_2_vec():
-    import gensim.downloader as api
-    info = api.info()  # show info about available models/datasets
+def pretrained_word_2_vec():
     model = api.load("glove-twitter-25")  # download the model and return as object ready for use
     # model = api.load("fasttext-wiki-news-subwords-300")
 
+def train_word_2_vec_model() -> Word2Vec:
+    tprint('Training Word2Vec Model...')
+    return Word2Vec(df['words'])
+
+def save_word_2_vec_model(model, name=MODEL_NAME):
+    tprint(f'Saving Word2Vec Model as {name}...')
+    model.save(name)
+
+def preprocess(col) -> pd.Series:
+    lemmatizer = WordNetLemmatizer()
+    words_to_remove = set(stopwords.words('english'))
+    words_to_remove.add('')
+    return col.str.lower().str.split(r"(?<=\w|-)[^\w-]+", regex=True).apply(
+        lambda sentence: [
+            lemmatizer.lemmatize(word, wordnet_tag(nltk_tag))
+            for word, nltk_tag in nltk.pos_tag(sentence)
+            if word not in words_to_remove and not word.isnumeric() and len(word) > 1
+        ]
+    )
+
+def keyword_helpful_count(df, keyword, print=False):
+    keyword_df = df.loc[df['words_set'].apply(lambda words: keyword in words)]
+    genuine_count = keyword_df['genuine'].sum()
+    fraud_count = sum(keyword_df['genuine'] == 0)
+    if print == 'latex':
+        print(fr"{keyword[2:]} & {len(keyword_df)} ({round(len(keyword_df) / len(df) * 100, 2)}\%) & {genuine_count} & {fraud_count} & {round(genuine_count / fraud_count, 2)}:1")
+    elif print is True:
+        print(f"{{{keyword}}}: Keyword Count: {len(keyword_df)} ({len(keyword_df)/len(df)}); Genuine Count: {genuine_count} ({genuine_count/len(df)}); Fraud Count: {fraud_count} ({fraud_count/len(df)}); ratio: {genuine_count/fraud_count}:1")
+    else:
+        return len(keyword_df), genuine_count, fraud_count, (genuine_count/fraud_count) if fraud_count else None
+
+def common_words(df, topn=1000):
+    counter = Counter(word for words in df['words'] for word in words)
+    return counter.most_common(topn)
+
+def important_keywords(df, ratio_threhold=10, topn=50):
+    tprint('Extracting Important Keywords...')
+    keywords = []
+    for word, count in common_words(df):
+        keyword_count, genuine_count, fraud_count, ratio = keyword_helpful_count(df, word)
+        if ratio is None or ratio > ratio_threhold:
+            keywords.append(word)
+            if len(keywords) >= topn:
+                break
+    return keywords
+
+def load_dataset():
+    tprint('Loading Dataset...')
+
+    try:
+        tprint('Try loading from cache...')
+        df = pd.read_csv(f"df_{DATASET}_size_{DATASET_SIZE}")
+    except:
+        tprint('Failed to load from cache...')
+
+        ds, info = tfds.load(f'amazon_us_reviews/{DATASET}', split='train', with_info=True)
+
+        tprint('Creating DataFrame...')
+        df = pd.DataFrame(tfds.as_dataframe(ds.take(DATASET_SIZE), info))
+        df = df.rename({col: col.replace('data/', '') for col in df.columns}, axis=1)
+
+        tprint('Decoding...')
+        df['review_body'] = df['review_body'].str.decode('utf-8')
+
+        tprint('Preprocessing Words...')
+        df['words'] = preprocess(df["review_body"])
+
+        tprint('Creating Words Sets...')
+        df['words_set'] = df['words'].apply(set)
+
+        df['genuine'] = None
+        genuine = (df['helpful_votes'] / df['total_votes']) >= GENUINE_THRESHOLD
+        fraud = (df['helpful_votes'] / df['total_votes']) <= FRAUDULENT_THRESHOLD
+        df.loc[genuine, 'genuine'] = 1
+        df.loc[fraud, 'genuine'] = 0
+
+        try:
+            df.to_csv(f"df_{DATASET}_size_{DATASET_SIZE}")
+            tprint("Successfully saved DataFrame")
+        except:
+            tprint("Failed to save DataFrame")
+
+    tprint('Fetching Target DataFrame...')
+    genuine = (df['helpful_votes'] / df['total_votes']) >= GENUINE_THRESHOLD
+    fraud = (df['helpful_votes'] / df['total_votes']) <= FRAUDULENT_THRESHOLD
+    target_df = df.loc[genuine | fraud]
+    genuine_ratio = sum(genuine) / sum(fraud)
+    tprint("Size of Dataset", df.shape)
+    tprint(f"Genuine Count: {sum(genuine)} ({sum(genuine) / len(target_df)}); Fraud Count: {sum(fraud)} ({sum(fraud) / len(target_df)}); ratio: {genuine_ratio}")
+
+    return df, target_df, genuine_ratio
+
+
 if __name__ == '__main__':
-    using_gensim_lda()
+    df, target_df, genuine_ratio = load_dataset()
+    print(important_keywords(df, genuine_ratio*3))
+    model = train_word_2_vec_model()
+    save_word_2_vec_model(model)
+    tprint('end')
