263091d6f9fe08cece84aabd74b156fc0992eb91
derek.yuen@wizpresso.com <6aa8a22d0ae819b52740c03a6a56b016411c581b>
2022-08-03 01:11:44 +0800

    classic GNN models

--- model_GNN.py
+++ model_GNN.py
@@ -10,43 +10,37 @@ from torch.nn import init
 """
 
 
-class OneLayerCARE(nn.Module):
+class GNN(nn.Module):
 	"""
 	The CARE-GNN model in one layer
 	"""
 
-	def __init__(self, num_classes, inter1, lambda_1):
+	def __init__(self, num_classes, inter1):
 		"""
 		Initialize the CARE-GNN model
 		:param num_classes: number of classes (2 in our paper)
 		:param inter1: the inter-relation aggregator that output the final embedding
 		"""
-		super(OneLayerCARE, self).__init__()
+		super(GNN, self).__init__()
 		self.inter1 = inter1
 		self.xent = nn.CrossEntropyLoss()
 
 		# the parameter to transform the final embedding
 		self.weight = nn.Parameter(torch.FloatTensor(inter1.embed_dim, num_classes))
 		init.xavier_uniform_(self.weight)
-		self.lambda_1 = lambda_1
 
-	def forward(self, nodes, labels, train_flag=True):
-		embeds1, label_scores = self.inter1(nodes, labels, train_flag)
+	def forward(self, nodes):
+		embeds1, label_scores = self.inter1(nodes)
 		scores = torch.mm(embeds1, self.weight)
 		return scores, label_scores
 
-	def to_prob(self, nodes, labels, train_flag=True):
-		gnn_scores, label_scores = self.forward(nodes, labels, train_flag)
+	def to_prob(self, nodes):
+		gnn_scores, label_scores = self.forward(nodes)
 		gnn_prob = nn.functional.softmax(gnn_scores, dim=1)
 		label_prob = nn.functional.softmax(label_scores, dim=1)
 		return gnn_prob, label_prob
 
-	def loss(self, nodes, labels, train_flag=True):
-		gnn_scores, label_scores = self.forward(nodes, labels, train_flag)
-		# Simi loss, Eq. (4) in the paper
-		label_loss = self.xent(label_scores, labels.squeeze())
-		# GNN loss, Eq. (10) in the paper
+	def loss(self, nodes, labels):
+		gnn_scores, label_scores = self.forward(nodes)
 		gnn_loss = self.xent(gnn_scores, labels.squeeze())
-		# the loss function of CARE-GNN, Eq. (11) in the paper
-		final_loss = gnn_loss + self.lambda_1 * label_loss
-		return final_loss
+		return gnn_loss
